241d7ff4ecb184e2fd241ee4a8fa5b598f325565
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-05 15:07:27 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AMI=ami-0f3c7d07486cad13
+AMI=ami-0f3c7d07486cad139
 SG_ID=sg-04bb94f5d828fa09d
 INSTANCES=("mongodb" "catalogue" "user" "cart" "redis" "shipping" "rabbitmq" "payment" "mysql" "web" "dispatch")
 
@@ -12,7 +12,6 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d  --tag-specification "ResourceType=instance,Tags=[{key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d --tag-specification "ResourceType=instance,Tags=[{key=Name,Value=$i}]"
 done
 
