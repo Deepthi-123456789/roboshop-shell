0c668bb8eadfddd91630ff8f73f7ac17a9c0eea6
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-05 16:28:27 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -13,6 +13,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d -tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0]. PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d --query 'Instances[0]. PrivateIpAddress' --output text)
     echo "$i:$IP_ADDRESS"
 done
