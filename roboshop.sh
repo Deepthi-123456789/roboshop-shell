fa94d00d72e20e32a1dd5deb783b3b66e657ef67
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-05 13:21:43 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ do
     echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
-        INSTANCE_TYPE ="t3.small"
+        INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE ="t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
 
     aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d
