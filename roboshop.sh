ac4b4c40a7e6fe5732621ca192f279a673cebc12
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-05 16:22:34 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -13,6 +13,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d -tags-specifications  —tags Key=Name, value=$i
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d -tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
 
