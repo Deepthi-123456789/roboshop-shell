7bb65f55dba147e47e21917114d3eee218849d9d
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-05 13:15:04 +0530

    first

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IAM_ID=ami-0f3c7d07486cad13
+SG_ID=sg-04bb94f5d828fa09d
+INSTANCES=("mongodb" "catalogue" "user" "cart" "redis" "rabbitmq" "payment" "mysql" "payment" "web" "dispatch")
+
+for i in "${INSTANCE[@]}"
+do
+    echo "instance is: $i"
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE ="t3.small"
+    else
+        INSTANCE_TYPE ="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d
+done
+
