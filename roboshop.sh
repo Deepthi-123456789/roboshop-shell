46b8e6906901d207ea61277f592b8358af8527f9
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-05 13:32:29 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 IAM_ID=ami-0f3c7d07486cad13
 SG_ID=sg-04bb94f5d828fa09d
-INSTANCES=("mongodb" "catalogue" "user" "cart" "redis" "rabbitmq" "payment" "mysql" "payment" "web" "dispatch")
+INSTANCES=("mongodb" "catalogue" "user" "cart" "redis" "shipping" "rabbitmq" "payment" "mysql" "web" "dispatch")
 
 for i in "${INSTANCE[@]}"
 do
@@ -14,6 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad13 --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d  --tag-specification "ResourceType=instance,Tags=[{key=Name,Value=$i}]"
 done
 
