8cae3f19c05bb10d0f54f79dea368f892b327a8a
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-20 12:26:44 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -19,7 +19,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d
      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
-
+done
 #     #create R53 record, make sure you delete existing record
 #     aws route53 change-resource-record-sets \
 #     --hosted-zone-id $ZONE_ID \
@@ -39,4 +39,4 @@ do
 #         }]
 #     }
 #         '
-# done
+#done
