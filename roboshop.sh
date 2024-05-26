f48922796dc17801e38cbf35eb4736bc9362d4ac
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-26 12:40:55 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/bang
 
-AMI=ami-0f3c7d07486cad139 #this keeps on changing
-SG_ID=sg-04bb94f5d828fa09d #replace with your SG ID
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z0426536RLNV1FOO62SE # replace your zone ID
-DOMAIN_NAME="pjdevops.online"
-
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-04bb94f5d828fa09d
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "shipping" "payment" "cart" "catalogue" "user" "web" "dispatch")
+ZONE_ID=Z08780431GOB4T1TR5RPR
+DOMAIN_NAME=pjdevops.online
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -16,6 +14,26 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-   aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-04bb94f5d828fa09d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
-    
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }'
 done
+
